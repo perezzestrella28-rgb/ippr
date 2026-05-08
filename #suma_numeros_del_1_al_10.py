647c63f5a2569746586b7b836af7fe4009a21056
perezzestrella28-rgb <perezzestrella28@gmail.com>
2026-05-07 20:01:57 -0600

    Add files via upload

---
+++ #suma_numeros_del_1_al_10.py
@@ -0,0 +1,10 @@
+#suma_numeros_del_1_al_10
+
+suma = 0
+
+for numero in range(1, 11):
+    print("numero actual:", numero)
+    suma = suma + numero
+    print("suma acumulada:", suma)
+
+print("La suma total del 1 al 10 es:" ,suma)
